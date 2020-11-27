80c9609d4a39ad378c3643ad4ff52168124a4e92
Álvaro Vargas Molina <36602950+AlvaroVargasM@users.noreply.github.com>
2020-11-26 21:26:27 -0600

    Added the main structure for the project

--- XTECDigital-front-end/src/app/app.module.ts
+++ XTECDigital-front-end/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AdminComponent } from './admin/admin/admin.component';
+import { ProfessorComponent } from './professor/professor/professor.component';
+import { StudentComponent } from './student/student/student.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    ProfessorComponent,
+    StudentComponent
   ],
   imports: [
     BrowserModule
