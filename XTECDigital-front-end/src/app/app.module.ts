f07adb4fbe42c338669241fac4cadcfd99f0772d
Álvaro Vargas Molina <36602950+AlvaroVargasM@users.noreply.github.com>
2020-11-30 18:53:19 -0600

    Minor changes to the names of the main view files

--- XTECDigital-front-end/src/app/app.module.ts
+++ XTECDigital-front-end/src/app/app.module.ts
@@ -2,10 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { AdminComponent } from './admin/admin/admin.component';
-import { ProfessorComponent } from './professor/professor/professor.component';
-import { StudentComponent } from './student/student/student.component';
-import { LoginComponent } from './home/login/login.component';
+import { AdminComponent } from './admin-view/admin/admin.component';
+import { ProfessorComponent } from './professor-view/professor/professor.component';
+import { StudentComponent } from './student-view/student/student.component';
+import { LoginComponent } from './home-view/login/login.component';
 import { RouterModule, Routes } from '@angular/router';
 import {FormsModule} from '@angular/forms';
 
