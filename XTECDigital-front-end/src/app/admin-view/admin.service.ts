d10e4ebbf0686e8895ca53f591699bf75ed937eb
Álvaro Vargas Molina <36602950+AlvaroVargasM@users.noreply.github.com>
2020-12-02 14:31:17 -0600

    Made the initial view of the admin page

--- XTECDigital-front-end/src/app/admin-view/admin.service.ts
+++ XTECDigital-front-end/src/app/admin-view/admin.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import {Course} from '../models/course.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminService {
+  // COURSE MANAGEMENT
 
-  constructor() { }
+  coursesAvailable: Course[];
+
+  constructor() {
+    this.coursesAvailable = [
+      new Course(
+        'CE3101',
+        'Bases de Datos',
+        4,
+        'Ingenieria en Computadores'),
+      new Course(
+        'CE2201',
+        'Laboratorio de Circuitos Electricos',
+        1,
+        'Ingenieria en Computadores'),
+      new Course(
+        'EL2207',
+        'Elementos Activos', 4,
+        'Ingenieria en Electronica'
+      )
+    ];
+  }
 }
--- XTECDigital-front-end/src/app/admin-view/admin/course-management/course-management.component.ts
+++ XTECDigital-front-end/src/app/admin-view/admin/course-management/course-management.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {AdminService} from '../../admin.service';
+import {Course} from '../../../models/course.model';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-course-management',
@@ -6,10 +9,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./course-management.component.css']
 })
 export class CourseManagementComponent implements OnInit {
+  @ViewChild('newCourse') newCourseForm: NgForm;
+  courses: Course[];
 
-  constructor() { }
+  constructor(private aService: AdminService) { }
 
   ngOnInit(): void {
+    this.courses = this.aService.coursesAvailable;
   }
 
+  onCreateCourse(): void {
+    console.log(this.newCourseForm.value.name);
+  }
 }
