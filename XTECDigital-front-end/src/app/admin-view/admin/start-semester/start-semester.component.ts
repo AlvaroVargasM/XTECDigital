5404913301fc98ea8e3ffa599f8ffc60d5a7a711
Álvaro Vargas Molina <36602950+AlvaroVargasM@users.noreply.github.com>
2020-12-03 00:05:29 -0600

    Added a way for moving between the course management and start semester components

---
+++ XTECDigital-front-end/src/app/admin-view/admin/start-semester/start-semester.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-start-semester',
+  templateUrl: './start-semester.component.html',
+  styleUrls: ['./start-semester.component.css']
+})
+export class StartSemesterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
