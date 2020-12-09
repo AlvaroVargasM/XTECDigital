2aa6c5501065b2e661ee8f78e263cebc445891c8
Álvaro Vargas Molina <36602950+AlvaroVargasM@users.noreply.github.com>
2020-12-09 13:25:52 -0600

    Works on initialize semester

--- XTECDigital-front-end/src/app/admin-view/admin/start-semester/start-semester.component.ts
+++ XTECDigital-front-end/src/app/admin-view/admin/start-semester/start-semester.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {Course} from '../../../models/course.model';
+import {AdminService} from '../../admin.service';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-start-semester',
@@ -6,10 +9,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./start-semester.component.css']
 })
 export class StartSemesterComponent implements OnInit {
+  @ViewChild('selectCourses') SelectCoursesForm: NgForm;
+  courses: Course[];
 
-  constructor() { }
+  constructor(private aService: AdminService) { }
 
   ngOnInit(): void {
+    this.courses = this.aService.coursesAvailable;
+  }
+
+  onSelectCourses(): void {
+    for (const property in this.SelectCoursesForm.value.courses) {
+      console.log(this.SelectCoursesForm.value.courses[property]);
+    }
   }
 
 }
