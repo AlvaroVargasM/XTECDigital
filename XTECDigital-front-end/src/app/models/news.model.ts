ec252fa350264f856129dba1bb18cd570abb7791
Álvaro Vargas Molina <36602950+AlvaroVargasM@users.noreply.github.com>
2020-12-17 12:44:12 -0600

    Added news to the professor view, can be displayed, created and deleted, editing pending

---
+++ XTECDigital-front-end/src/app/models/news.model.ts
@@ -0,0 +1,9 @@
+export class News {
+  constructor(
+    public author: string,
+    public title: string,
+    public message: string,
+    public date: string
+  ) {
+  }
+}
--- XTECDigital-front-end/src/app/professor-view/professor.service.ts
+++ XTECDigital-front-end/src/app/professor-view/professor.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
 import {Rubric} from '../models/rubric.model';
+import {News} from '../models/news.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfessorService {
   rubrics: Rubric[];
+  news: News[];
 
   constructor() {
     this.rubrics = [
@@ -20,6 +22,20 @@ export class ProfessorService {
       new Rubric(
         'Proyectos',
         40
+      )
+    ];
+    this.news = [
+      new News(
+        '',
+        'Ya esta el segundo quiz',
+        'Ya se encuentra disponible la evaluacion del quiz #2',
+        '20-12-2019'
+      ),
+      new News(
+        '',
+        'Se atrasa la tarea',
+        'La tarea que estaba para este lunes se va a mover hasta nuevo asviso por motivos personales',
+        '18-9-2020'
       )
     ];
   }
