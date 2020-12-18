44edacd041a87591de1a0a659d070f466df56498
jonDorito <36711268+jonDorito@users.noreply.github.com>
2020-12-18 06:10:36 -0600

    Added debug controller

---
+++ XTECDigital-RelationalDB-RESTAPI/RelationalDB_RESTAPI/Controllers/DebugController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace RelationalDB_RESTAPI.Controllers
+{
+    [RoutePrefix("Debug")]
+    public class DebugController : ApiController
+    {
+        [HttpGet]
+        [Route("getPath")]
+        public string getPath()
+        {
+            return AppDomain.CurrentDomain.BaseDirectory;
+        }
+    }
+}
